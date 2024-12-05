5ab4ceb506a6b1480a2f4130858a9b0737ec13cb
Shangqi Xue <shangqixue@gmail.com>
2024-12-04 20:23:07 -0800

    Added comment and deleted useless code

--- Assets/Scripts/ChangeStatus.cs
+++ Assets/Scripts/ChangeStatus.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeStatus : MonoBehaviour
@@ -7,24 +5,15 @@ public class ChangeStatus : MonoBehaviour
 
     private CameraController ccontroller; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// When collision happens, change the alive status to false and call spectate mode 
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.name != "Floor"){
             ccontroller = other.gameObject.GetComponentInParent<CameraController>();
             ccontroller.alive = false;
             ccontroller.Spectate();
-            Debug.Log(other.gameObject.name);
         }
     }
 }
