afecfd109da2ef8d1ed3b797de0b28e6e7f2a02e
Shangqi Xue <shangqixue@gmail.com>
2024-12-11 00:36:10 -0800

    fixed all issue with items
    There was some issues with items and network. For instance items would not show to be equipped on the client when the host can see it. Basically fixed all item issue with networking and item authority situation . Also made some changes to the format of the weapon and armor class.

--- Assets/Scripts/Items/Armor.cs
+++ Assets/Scripts/Items/Armor.cs
@@ -7,7 +7,7 @@ using Mirror;
 /// Represents an armor piece that can be equipped by an entity.
 /// Gives defensive capabilities and special effects.
 /// </summary>
-public class Armor : NetworkBehaviour
+public class Armor : NetworkBehaviour, IInventoryItem
 {
     public enum ArmorType
     {
@@ -30,28 +30,17 @@ public class Armor : NetworkBehaviour
     public string SpecialEffect => specialEffect;
     public bool IsEquipped => isEquipped;
     public ArmorType Type => armorType;
+
     /// <summary>
     /// Equips the armor piece by attaching it to the specified equip point
     /// </summary>
-    /// <param name="equipPoint"></param>
-    [Command]
-    public void CmdEquip(Transform equipPoint)
+    /// <param name="gameObject">gameObject where the equip point can be found</param>
+    public void Interact(GameObject gameObject)
     {
-        RpcEquip(equipPoint);
-    }
-
-    /// <summary>
-    /// Synchronizes the armor's equipped state visually across all clients
-    /// </summary>
-    /// <param name="equipPoint"></param>
-    [ClientRpc]
-    private void RpcEquip(Transform equipPoint)
-    {
+        Transform equipPoint = gameObject.transform.Find("headEquipPoint");
         transform.SetParent(equipPoint);
         transform.localPosition = Vector3.zero;
         transform.localRotation = Quaternion.identity;
-
-        Debug.Log($"{armorName} equipped on {equipPoint.name}.");
         isEquipped = true;
     }
 
@@ -59,10 +48,22 @@ public class Armor : NetworkBehaviour
     /// Unequips the armor piece and detaches it from its parent
     /// </summary>
     [Command]
-    public void CmdUnequip()
-    {
+    public void CmdUnequip(){
+        NetworkIdentity armorIdentity = GetComponent<NetworkIdentity>();
+        if (armorIdentity != null)
+        {
+            armorIdentity.RemoveClientAuthority();
+        }
         transform.SetParent(null);
-        Debug.Log($"{armorName} unequipped.");
+        RpcUnequip();
+    }
+
+    /// <summary>
+    /// Unequips the armor piece to reflect on all clients
+    /// </summary>
+    [ClientRpc] 
+    private void RpcUnequip(){
+        transform.SetParent(null);
     }
 
     /// <summary>
