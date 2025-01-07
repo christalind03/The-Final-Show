a710cac1c7953f740b03dee243c392e3ea1a2599
Christalin Dorsey <christalin.dorsey03@gmail.com>
2025-01-06 16:16:04 -0800

    Implement universal debugging log for user interface components using UI Toolkit

--- Assets/Scripts/Utils/UnityUtils.cs
+++ Assets/Scripts/Utils/UnityUtils.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// A static utility class containing helper methods for Unity related tasks.
@@ -17,36 +18,64 @@ public static class UnityUtils
         return layerMask == (layerMask | (1 << targetLayer));
     }
 
+    // TODO: Document
+    public static bool ContainsElement<TElement>(VisualElement rootVisualElement, string targetElement, out TElement uiElement) where TElement : VisualElement
+    {
+        if (rootVisualElement == null)
+        {
+            uiElement = null;
+            return false;
+        }
+        else
+        {
+            uiElement = rootVisualElement.Query<TElement>(targetElement);
+
+            if (uiElement != null)
+            {
+                return true;
+            }
+            else
+            {
+                LogError($"Unable to locate {typeof(TElement)} titled '{targetElement}'", 3);
+                return false;
+            }
+        }
+
+    }
+
+    // TODO: Update Documentation
     /// <summary>
     /// Logs an error message to the Unity console, prefixed with the script's name.
     /// </summary>
     /// <param name="errorMessage">The error message to log.</param>
-    public static void LogError(string errorMessage)
+    public static void LogError(string errorMessage, int frameIndex = 1)
     {
-        string scriptName = FetchScriptName();
+        string scriptName = FetchScriptName(frameIndex);
 
         UnityEngine.Debug.LogError($"[{scriptName}] {errorMessage}");
     }
 
+    // TODO: Update Documentation
     /// <summary>
     /// Logs a warning message to the Unity console, prefixed with the script's name.
     /// </summary>
     /// <param name="warningMessage">The warning message to log.</param>
-    public static void LogWarning(string warningMessage)
+    public static void LogWarning(string warningMessage, int frameIndex = 1)
     {
-        string scriptName = FetchScriptName();
+        string scriptName = FetchScriptName(frameIndex);
 
         UnityEngine.Debug.LogWarning($"[{scriptName}] {warningMessage}");
     }
 
+    // TODO: Update Documentation
     /// <summary>
     /// Retrieves the name of the current script.
     /// </summary>
     /// <returns>The name of the script calling the current method.</returns>
-    private static string FetchScriptName()
+    private static string FetchScriptName(int frameIndex)
     {
         StackTrace stackTrace = new StackTrace();
-        StackFrame stackFrame = stackTrace.GetFrame(1);
+        StackFrame stackFrame = stackTrace.GetFrame(frameIndex);
         string scriptName = stackFrame.GetMethod().DeclaringType.Name;
 
         return scriptName;
