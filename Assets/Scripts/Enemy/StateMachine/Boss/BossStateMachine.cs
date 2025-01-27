8d67b8e974ef3b0c086c3ccef3ee61dea211b0af
Glenn-John <john.glenn104@gmail.com>
2025-01-26 18:14:46 -0800

    Boss overrides base transition logic

--- Assets/Scripts/Enemy/StateMachine/Boss/BossStateMachine.cs
+++ Assets/Scripts/Enemy/StateMachine/Boss/BossStateMachine.cs
@@ -30,6 +30,7 @@ public class BossStateMachine : EnemyStateMachine
     /// <summary>
     /// Contains logic for keeping track of the target and transitioning between states
     /// </summary>
+    /*
     protected override void FixedUpdate()
     {
         // _fieldOfView's interested layers should only be player
@@ -139,6 +140,101 @@ public class BossStateMachine : EnemyStateMachine
         else
         {
             TransitionToState(EEnemyState.Idle);
+        }
+    }
+    */
+
+    protected override void TransitionLogic()
+    {
+        float distToTarget = 0f;
+        // if we're in the middle of using an ability, do nothing
+        if (_usingAbility)
+        {
+            return;
+        }
+        // if we're not using an ability anymore but we're still in the ability state
+        else if (CurrentState.StateKey.Equals(EEnemyState.Ability1))
+        {
+            // If we still have a target, start Chasing
+            if (_hasTarget)
+            {
+                TransitionToState(EEnemyState.Chasing);
+            }
+            // otherwise, go Idle
+            else
+            {
+                TransitionToState(EEnemyState.Idle);
+            }
+        }
+        // use push ability if able and there are players in the FOV
+        if (_canAbility1 && _fieldOfView.DetectedObjects.Count > 0)
+        {
+            TransitionToState(EEnemyState.Ability1);
+            _canAbility1 = false;
+            _usingAbility = true;
+            StartCoroutine(Ability1Cooldown());
+            StartCoroutine(Ability1Duration());
+        }
+        else if (_hasTarget)
+        {
+            distToTarget = Vector3.Distance(transform.position, _targetTransform.position); // recalculate distToTarget
+            // If Idle and target is closer than startChaseDist, start chasing
+            if (CurrentState.StateKey.Equals(EEnemyState.Idle) && distToTarget < _behaviorStats.StartChaseDist)
+            {
+                TransitionToState(EEnemyState.Chasing);
+            }
+            // If Chasing...
+            else if (CurrentState.StateKey.Equals(EEnemyState.Chasing))
+            {
+                // and target is farther than endChaseDist, stop chasing, no longer have a target
+                if (distToTarget > _behaviorStats.EndChaseDist)
+                {
+                    _hasTarget = false;
+                }
+                // and target is closer than startAimDist, start Aiming
+                else if (distToTarget < _behaviorStats.StartAimDist)
+                {
+                    TransitionToState(EEnemyState.Aiming);
+                }
+            }
+            // If Aiming...
+            else if (CurrentState.StateKey.Equals(EEnemyState.Aiming))
+            {
+                // and target is farther than endAimDist, start Chasing
+                if (CurrentState.StateKey.Equals(EEnemyState.Aiming) && distToTarget > _behaviorStats.EndAimDist)
+                {
+                    TransitionToState(EEnemyState.Chasing);
+                }
+                // and can attack, Attack
+                else if (CurrentState.StateKey.Equals(EEnemyState.Aiming) && _canAttack)
+                {
+                    TransitionToState(EEnemyState.Attacking);
+                    _canAttack = false;
+                    StartCoroutine(AttackCooldown());
+                    // TODO: add a delay here for duration of attack animation
+                }
+            }
+            // If Attacking but can no longer attack, start Aiming
+            else if (CurrentState.StateKey.Equals(EEnemyState.Attacking) && !_canAttack)
+            {
+                TransitionToState(EEnemyState.Aiming);
+            }
+        }
+        // If we don't have a target anymore, scan for a new one
+        if (!_hasTarget)
+        {
+            if (_fieldOfView.DetectedObjects.Count > 0)
+            {
+                _hasTarget = true;
+                // TODO: The first object in the list seems to always be the host, so maybe find the closest obj in the list
+                _targetTransform = _fieldOfView.DetectedObjects[0].transform; // get the first object
+                StateContext.TargetTransform = _targetTransform;
+            }
+            // No target and couldn't find a new one -> go to Idle
+            else
+            {
+                TransitionToState(EEnemyState.Idle);
+            }
         }
     }
 
