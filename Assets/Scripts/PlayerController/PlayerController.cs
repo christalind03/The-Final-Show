d61ed93ee2860150a8bb52aa37c27cb9a51c5488
Shangqi Xue <shangqixue@gmail.com>
2025-02-04 20:28:41 -0800

    deleted testing code

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -399,7 +399,6 @@ public class PlayerController : NetworkBehaviour
     /// When the player press Tab, the player list will open/close based on the current display status
     /// </summary>
     private void ScoreBoard(InputAction.CallbackContext context){
-        _scoreboard.CmdUpdateKillData(netId, 1, 1);
         _playerInterface.RefreshScoreBoard();
         _playerInterface.ToggleScoreBoardVisibility();
     }
