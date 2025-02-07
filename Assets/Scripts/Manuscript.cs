673628106085d7b68836f1e38085a92531312d87
Glenn-John <john.glenn104@gmail.com>
2025-02-07 15:22:07 -0800

    renamed XPScript > Manuscript

--- Assets/Scripts/Manuscript.cs
+++ Assets/Scripts/Manuscript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class XPScript : NetworkBehaviour
+public class Manuscript : NetworkBehaviour
 {
     
     private GameplayManager gameplayManager;
