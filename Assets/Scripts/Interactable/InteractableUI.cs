13bd8b17d3dac6606772a1036f5557890a8a4dbd
Shangqi Xue <shangqixue@gmail.com>
2025-02-11 17:42:27 -0800

    added key icons and change enable for  inter ui

--- Assets/Scripts/Interactable/InteractableUI.cs
+++ Assets/Scripts/Interactable/InteractableUI.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class InteractableUI : AbstractBillboard
 {
     [Header("Message to display")]
     [SerializeField] private string message;
-    [SerializeField] private float _interactableDistance;
-    private GameObject _player;
-    private Canvas canvas;
 
-    private void Start() {
-        canvas = GetComponentInParent<Canvas>();    
+    [Header("References")]
+    [SerializeField] private float _interactableDistance;
+    [SerializeField] private Text _infoText;
+    [SerializeField] private Canvas canvas;
+    private GameObject _player;
+    private Camera _camera;
+    private RaycastHit _raycastHit;
+
+    private void Start() {  
+        _infoText.text = message;
     }
     
     protected override void LateUpdate()
@@ -18,14 +25,19 @@ public class InteractableUI : AbstractBillboard
 
         if(_player == null && NetworkUtils.RetrieveLocalPlayer() != null){
             _player = NetworkUtils.RetrieveLocalPlayer();
+            _camera = _player.GetComponentInChildren<Camera>();
         }
         
-        if(_player == null) return;
-
-        if(Vector3.Distance(transform.position, _player.transform.position) > _interactableDistance){
-            canvas.enabled = false;
+        if(_player == null || _camera == null) return;
+        
+        Physics.Raycast(_camera.transform.position, _camera.transform.forward, out _raycastHit, _interactableDistance);
+        if(_raycastHit.collider != null && _raycastHit.collider.gameObject.GetComponentInChildren<InteractableUI>() != null){
+            InteractableUI hitUI = _raycastHit.collider.gameObject.GetComponentInChildren<InteractableUI>();
+            if(hitUI == this){
+                canvas.enabled = true;
+            }
         }else{
-            canvas.enabled = true;
+            canvas.enabled = false;
         }
     }
 }
