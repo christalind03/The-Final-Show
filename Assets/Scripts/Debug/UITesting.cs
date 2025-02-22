0fb6a54a0db0f03457066fc2bc55fcd4a194e942
Shangqi Xue <shangqixue@gmail.com>
2025-02-21 17:35:44 -0800

    working animation
    need to figure out how to dynamically change the reference object

--- Assets/Scripts/Debug/UITesting.cs
+++ Assets/Scripts/Debug/UITesting.cs
@@ -1,50 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Mono.CecilX;
+using Org.BouncyCastle.Asn1.Crmf;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 using UnityEngine.UIElements;
 
 public class UITesting : MonoBehaviour
-{
-    [SerializeField] PlayableDirector openMenu;
+{    
+    private const string GeneralTab = "Setting-General-Container";
+    private const string AudioTab = "Setting-Audio-Container";
+    private const string ControlsTab = "Setting-Controls-Container";
+    [SerializeField] PlayableDirector menuOpenClose;
     [SerializeField] PlayableDirector switchSettingTabs;
-    [SerializeField] PlayableDirector showAudioTab;
+    [SerializeField] PlayableDirector showTab;
     [SerializeField] UIDocument uIDocument;
+    private Dictionary<string, VisualElement> tabElements = new Dictionary<string, VisualElement>();
+    private Dictionary<string, TimelineAsset> timelines = new Dictionary<string, TimelineAsset>();
     private VisualElement _rootVisualElement;
+    private VisualElement _settingsMenu;
     private VisualElement _navContainer;
-    private VisualElement _generalContainer;
-    private VisualElement _audioContainer;
-    private VisualElement _controlsContainer;
     private string currentTab;
+    private bool isOpen;
+
 
     void Start(){
+        TimelineAsset[] loadedTimelines = Resources.LoadAll<TimelineAsset>("UI/Timeline/");
+        foreach (TimelineAsset timeline in loadedTimelines){
+            timelines.Add(timeline.name, timeline);
+        }
+
+        isOpen = false;
         _rootVisualElement = uIDocument.rootVisualElement;
         currentTab = "Setting-General-Container";
         if(UnityUtils.ContainsElement(_rootVisualElement, "Settings-Container", out VisualElement settings)){
+            _settingsMenu = settings;
             if(UnityUtils.ContainsElement(settings, "Setting-Nav-Container", out VisualElement nav)){
                 _navContainer = nav;
             }
-            if(UnityUtils.ContainsElement(settings, "Setting-General-Container", out VisualElement gen)){
-                _generalContainer = gen;
-            }
-            if(UnityUtils.ContainsElement(settings, "Setting-Audio-Container", out VisualElement aud)){
-                _audioContainer = aud;
-            }
-            if(UnityUtils.ContainsElement(settings, "Setting-Controls-Container", out VisualElement con)){
-                _controlsContainer = con;
-            }
+            RegisterVisEle(settings, GeneralTab);
+            RegisterVisEle(settings, AudioTab);
+            RegisterVisEle(settings, ControlsTab);
         }
 
-        if(UnityUtils.ContainsElement(_navContainer, "Setting-GeneralBtn", out Button genBtn)){
-            genBtn.clicked += SwitchToGeneral;           
-        }
-        if(UnityUtils.ContainsElement(_navContainer, "Setting-AudioBtn", out Button audioBtn)){
-            audioBtn.clicked += SwitchToAudio;           
-        }
-        if(UnityUtils.ContainsElement(_navContainer, "Setting-ControlsBtn", out Button conBtn)){
-            conBtn.clicked += SwitchToControl;           
-        }
+        RegisterButton("Setting-GeneralBtn", () => SwitchTab(GeneralTab));
+        RegisterButton("Setting-AudioBtn", () => SwitchTab(AudioTab));
+        RegisterButton("Setting-ControlsBtn", () => SwitchTab(ControlsTab));
     }
 
     // Update is called once per frame
@@ -56,27 +60,84 @@ public class UITesting : MonoBehaviour
     }
 
     private void OpenMenu(){
-        _generalContainer.visible = true;
-        _audioContainer.visible = false;
-        _controlsContainer.visible = false;
-        openMenu.Play();
+        if(!isOpen){
+            menuOpenClose.playableAsset = timelines["OpenSettings"];
+            _settingsMenu.RemoveFromClassList("secondaryContainer");
+            _settingsMenu.AddToClassList("primaryContainer");
+            foreach(var elementsPair in tabElements){
+                if(elementsPair.Key != GeneralTab){
+                    elementsPair.Value.visible = false;
+                }else{
+                    elementsPair.Value.visible = true;
+                }
+            }
+            currentTab = GeneralTab;
+            tabElements[GeneralTab].visible = true;
+            menuOpenClose.Play();   
+            isOpen = true;
+        }else{
+            TimelineAsset closeTimeline = timelines["CloseSettings"];
+            menuOpenClose.playableAsset = closeTimeline;
+            tabElements[GeneralTab].visible = true;     
+            menuOpenClose.Play();   
+            isOpen = false;
+        }         
     }
-    private void SwitchToGeneral(){
-        currentTab = "Setting-Audio-Container";
-        _audioContainer.visible = true;
-        switchSettingTabs.Play();
-        showAudioTab.Play();
+    private void SwitchTab(string newTab){
+        if(currentTab != newTab){
+            if(switchSettingTabs.state == PlayState.Playing) return;
+            switch (currentTab){
+                case "Setting-General-Container":
+                    switchSettingTabs.playableAsset = timelines["SwitchGeneralTab"];
+                    break;
+                case "Setting-Audio-Container":
+                    switchSettingTabs.playableAsset = timelines["SwitchAudioTab"];
+                    break;
+                case "Setting-Controls-Container":
+                    switchSettingTabs.playableAsset = timelines["SwitchControlsTab"];
+                    break;
+                default:
+                    Debug.LogWarning("Invalid Tab");
+                    break;
+            }
+            switch (newTab){
+                case "Setting-General-Container":
+                    showTab.playableAsset = timelines["ShowGeneralTab"];
+                    break;
+                case "Setting-Audio-Container":
+                    showTab.playableAsset = timelines["ShowAudioTab"];
+                    break;
+                case "Setting-Controls-Container":
+                    showTab.playableAsset = timelines["ShowControlsTab"];
+                    break;
+                default:
+                    Debug.LogWarning("Invalid Tab");
+                    break;
+            }
+            switchSettingTabs.Play();
+            showTab.Play();
+            tabElements[currentTab].RemoveFromClassList(".settingActiveContainer");
+            foreach(string name in tabElements[currentTab].GetClasses()){
+                Debug.Log(currentTab + " " + name);
+            }
+            currentTab = newTab;
+            tabElements[currentTab].AddToClassList(".settingActiveContainer");
+            foreach(string name in tabElements[currentTab].GetClasses()){
+                Debug.Log(currentTab + " " +name);
+            }
+            tabElements[currentTab].visible = true; //show new tab
+        }
     }
-    private void SwitchToAudio(){
-        currentTab = "Setting-Audio-Container";
-        _audioContainer.visible = true;
-        switchSettingTabs.Play();
-        showAudioTab.Play();
+
+    private void RegisterVisEle(VisualElement root, string name){
+        if(UnityUtils.ContainsElement(root, name, out VisualElement output)){
+            tabElements[name] = output;
+            output.visible = false;
+        }
     }
-    private void SwitchToControl(){
-        currentTab = "Setting-Audio-Container";
-        _audioContainer.visible = true;
-        switchSettingTabs.Play();
-        showAudioTab.Play();
+    private void RegisterButton(string name, System.Action onClick){
+        if(UnityUtils.ContainsElement(_navContainer, name, out Button output)){
+            output.clicked += onClick;
+        }
     }
 }
