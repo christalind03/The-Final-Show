da5cd351ae25ed3fdad96c0f5f955cca96206081
Christalin Dorsey <christalin.dorsey03@gmail.com>
2025-02-28 14:00:43 -0800

    Enable audio mixer groups for `AudioAsset` scriptable objects

--- Assets/Scripts/Audio/AudioResource.cs
+++ Assets/Scripts/Audio/AudioResource.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "New Audio Resource", menuName = "Audio Resource")]
 public class AudioResource : ScriptableObject
 {
     public AudioClip AudioClip;
+    public AudioMixerGroup AudioMixerGroup;
 
     [Range(0f, 1f)] public float Volume = 0.5f;
     [Range(0.1f, 3f)] public float Pitch = 1f;
