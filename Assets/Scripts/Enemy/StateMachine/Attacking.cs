379db27abce8c638d91bd7313be4f04c9fe6091d
Glenn-John <john.glenn104@gmail.com>
2025-03-11 19:46:42 -0700

    added comments

--- Assets/Scripts/Enemy/StateMachine/Attacking.cs
+++ Assets/Scripts/Enemy/StateMachine/Attacking.cs
@@ -27,9 +27,14 @@ public class AttackingState : EnemyState
     public override void OnTriggerStay(Collider otherCollider) { }
     public override void UpdateState() { }
 
+    /// <summary>
+    /// Waits an amount of time given by AttackDelay for the animation to wind up, then deals damage to the player.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Attack()
     {
         yield return new WaitForSeconds(StateContext.AttackDelay);
+        // TODO: Check if the target is still in range after the wind-up
         if (StateContext.TargetTransform.root.TryGetComponent(out AbstractHealth targetHealth))
         {
             targetHealth.CmdDamage(StateContext.AttackStats.AttackDamage);
