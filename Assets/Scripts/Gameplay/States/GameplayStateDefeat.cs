e153fa2f066ea622d3c20b59aabd81163ffa5be3
Sarah Orner <sarah@orner.net>
2025-04-12 15:54:46 -0700

    defeat scene loads properly, now theme maps align with other theme elements

--- Assets/Scripts/Gameplay/States/GameplayStateDefeat.cs
+++ Assets/Scripts/Gameplay/States/GameplayStateDefeat.cs
@@ -1,9 +1,11 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "New Defeat Gameplay State", menuName = "Base State/Gameplay/Defeat")]
 public class GameplayStateDefeat : GameplayState
 {
+    private bool _sceneLoaded;
     public override void EnterState()
     {
         Debug.Log("Entering the DEFEAT gameplay state...");
@@ -13,12 +15,21 @@ public class GameplayStateDefeat : GameplayState
             steamLobby.SetSceneData("Defeat");         
         }
         base.EnterState();
-
-        //Spawns in the map for the correct theme
-        GameObject defeatRoom = Instantiate(StateContext.GameplayTheme.DefeatRoomPrefab, Vector3.zero, Quaternion.identity);
-        NetworkServer.Spawn(defeatRoom);
     }
 
+    /// <summary>
+    /// When the target scene is loaded, instantiates and spawns the map depending on the active theme.
+    /// </summary>
+    /// <param name="activeScene">The scene that was loaded</param>
+    /// <param name="loadMode">The mode in which the scene was loaded</param>
+    protected override void OnSceneLoaded(Scene activeScene, LoadSceneMode loadMode)
+    {
+        base.OnSceneLoaded(activeScene, loadMode);
+        GameObject defeatRoom = Instantiate(StateContext.GameplayTheme.DefeatRoomPrefab, Vector3.zero, Quaternion.identity);
+        NetworkServer.Spawn(defeatRoom);
+        _sceneLoaded = true;
+    
+    }
     public override void ExitState()
     {
         Debug.Log("Exiting the DEFEAT gameplay state...");
