a9609513a402e84a375c27b73b2a83776ed86434
Glenn-John <john.glenn104@gmail.com>
2025-04-18 18:37:38 -0700

    teleport ability spawns smoke clouds

--- Assets/Scripts/Enemy/StateMachine/Boss/TeleportAbility.cs
+++ Assets/Scripts/Enemy/StateMachine/Boss/TeleportAbility.cs
@@ -1,3 +1,4 @@
+using Mirror;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -9,9 +10,11 @@ public class TeleportAbilityState : EnemyState
     [SerializeField]
     [Tooltip("The distance from the player that the boss will teleport to")]
     private float _teleportDistance = 5f;
+    [SerializeField] GameObject _poofPrefab;
     public override void EnterState()
     {
         Debug.Log("Entering Teleport State");
+        StateContext.NavMeshAgent.isStopped = true;
         StateContext.Animator.SetBool("Is Teleporting", true);
         StateContext.MonoBehaviour.StartCoroutine(Teleport());
     }
@@ -29,6 +32,10 @@ public class TeleportAbilityState : EnemyState
 
     private IEnumerator Teleport()
     {
+        // Spawn the smoke slightly in front of the enemy since it takes a small amount of time for it to stop moving
+        Vector3 startLoc = StateContext.Transform.position + StateContext.Transform.forward * StateContext.NavMeshAgent.velocity.magnitude / 4; // 4 just feels like a good value
+        GameObject startPoof = Instantiate(_poofPrefab, startLoc, Quaternion.identity);
+        NetworkServer.Spawn(startPoof);
         yield return new WaitForSeconds(1.0f); // Wait 1 second for wind-up animation
         // Get a location behind the current target and warp the enemy there
         Vector3 dest = _teleportDistance * (StateContext.TargetTransform.position - StateContext.Transform.position).normalized;
@@ -36,6 +43,9 @@ public class TeleportAbilityState : EnemyState
         NavMeshHit hit;
         if (NavMesh.SamplePosition(dest, out hit, 1.0f, NavMesh.AllAreas))
         {
+            GameObject destPoof = Instantiate(_poofPrefab, hit.position, Quaternion.identity);
+            NetworkServer.Spawn(destPoof);
+            yield return new WaitForSeconds(0.5f); // Small delay to let some smoke spawn before actually warping
             StateContext.NavMeshAgent.Warp(hit.position);
             StateContext.Transform.LookAt(StateContext.TargetTransform);
         }
